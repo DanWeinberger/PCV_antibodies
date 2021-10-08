bc32138fc6426ebd3c42e7c76bf13f8360e00c4e
Weinberger <daniel.weinberger@yale.edu>
2021-10-07 22:19:07 -0400

    generalize code to allow 3+ vaccines

--- app.R
+++ app.R
@@ -7,7 +7,9 @@ library(plotly)
 
 pcv7sts <- c('4','6B','9V','14','18C','19F','23F')
 d1a <- read_excel("./Data/IgGGMCs2.xlsx")
-d1 <- d1a[d1a$Trial!='US2',]
+
+#d1 <- d1a[d1a$Trial!='US2',]
+d1 <- d1a
 names(d1)[1] <- 'vax'
 
 d1.m <- melt(d1, id.vars=c('vax','Dose','Trial','timepoint_dose_weeks'))
@@ -34,7 +36,9 @@ shinyApp(
       checkboxGroupInput("doses", "Doses:",
                          unique(d2$Dose), selected=c("3rddoseP" ,"Booster")),
       checkboxGroupInput("st", "Serotypes:",
-                         unique(d2$st),  selected=unique(d2$st))
+                         unique(d2$st),  selected=unique(d2$st)),
+      selectInput("ref_vax", "Vaccine to compare against:",
+                  unique(d2$vax), multiple=F, selected=unique(d2$vax)[1]),
      ),
     mainPanel(
       tabsetPanel(type = "tabs",
@@ -61,6 +65,7 @@ shinyApp(
           theme_classic()+
           ylab('log(GMC)') +
           facet_grid(Dose~st ) +
+          theme(axis.text.x=element_text(angle=90, hjust=1)) +
           theme(panel.spacing = unit(1.5, "lines"))
         )
 
@@ -73,16 +78,24 @@ shinyApp(
                          d2$st %in% input$st &
                          d2$Trial %in% input$country ) ,]
         plot.ds.c <- reshape2::dcast(plot.ds, Dose+Trial+st ~vax, value.var='gmc')
-        plot.ds.c <- plot.ds.c[!is.na(plot.ds.c$PCV13) & !is.na(plot.ds.c$PCV7),]
+        
+        vax.dat <- plot.ds.c[,input$vax, drop=F]
+        vax.dat.ratio <- as.data.frame(apply(vax.dat,2, function(x) x/vax.dat[,input$ref_vax]))
+        vax.dat.ratio <- vax.dat.ratio[, -grep(input$ref_vax, names(vax.dat.ratio) ), drop=F]
+        names(vax.dat.ratio) <- paste0('Numerator ', names(vax.dat.ratio))
+        
+        plot.ds.c2 <- cbind.data.frame(plot.ds.c[c('Dose','Trial','st')],vax.dat.ratio)
+        plot.ds.c2.m <- reshape2::melt(plot.ds.c2, id.vars=c('Dose','Trial','st'))
+
         p2 <- ggplotly(
-          ggplot(plot.ds.c, aes(x=st, y=log(PCV13/PCV7), group=Trial, col=Trial) ) +
+          ggplot(plot.ds.c2.m, aes(x=st, y=log(value), group=Trial, col=Trial) ) +
           geom_point() +
           theme_classic()+
-          ggtitle("Ratio of new vaccine to reference") +
+          ggtitle(paste0("Ratio of PCVx to ", input$ref_vax )) +
           ylab('log(Ratio)') +
           geom_line(aes(group = Trial),color="grey") +
           geom_hline(yintercept=0) +
-          facet_grid(Dose~. ) +
+          facet_grid(Dose~variable ) +
           theme(panel.spacing = unit(1, "lines"))
         )
         
