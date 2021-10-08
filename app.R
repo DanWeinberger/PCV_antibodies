f242f81bc10c0fd187df7fb34e5b54fdda0c5031
Weinberger <daniel.weinberger@yale.edu>
2021-10-07 21:30:01 -0400

    add tabs, and plotly

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(readxl)
 library(reshape2)
 library(shiny)
 library(ggplot2)
+library(plotly)
 
 pcv7sts <- c('4','6B','9V','14','18C','19F','23F')
 d1a <- read_excel("./Data/IgGGMCs2.xlsx")
@@ -20,6 +21,11 @@ names(d2) <- c('vax','Dose','Trial','time_dose', 'st','gmc')
 shinyApp(
   
   ui = fluidPage(
+    
+    titlePanel("Comparison of Immmunogenicity of PCVs"),
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+      
     sidebarPanel(
       selectInput("vax", "Vaccine:",
                   unique(d2$vax), multiple=T, selected=unique(d2$vax)),
@@ -28,56 +34,58 @@ shinyApp(
       checkboxGroupInput("doses", "Doses:",
                          unique(d2$Dose), selected=c("3rddoseP" ,"Booster")),
       checkboxGroupInput("st", "Serotypes:",
-                         unique(d2$st),  selected=unique(d2$st)),
-      selectInput('plot_type', 'Plot type:',
-                  c('Raw Concentration','Ratio'), selected= 'Raw Concentration') 
-    ),
+                         unique(d2$st),  selected=unique(d2$st))
+     ),
     mainPanel(
-      plotOutput("periodPlot")
-    )
+      tabsetPanel(type = "tabs",
+                  tabPanel("Concentration", plotlyOutput("plot_conc")),
+                  tabPanel("Ratio", plotlyOutput("plot_ratio"))
+      )
+      )
+  )
   ), 
   
   server = function(input, output) {
-    output$periodPlot = renderPlot({
+    output$plot_conc = renderPlotly({
       
-      if(input$plot_type=='Raw Concentration'){
-        
         plot.ds <- d2[(d2$vax %in% input$vax & 
                          d2$Dose %in% input$doses & 
                          d2$st %in% input$st &
                          d2$Trial %in% input$country ) ,]
         
-        p1 <- ggplot(plot.ds, aes(x=vax, y=log(gmc), group=vax, col=vax) ) +
+        p1 <-   ggplotly(
+          ggplot(plot.ds, aes(x=vax, y=log(gmc), group=vax, col=vax) ) +
           geom_point() +
+          ggtitle("Antibody concentration by product") +
           geom_line(aes(group = Trial),color="grey") +
           theme_classic()+
           ylab('log(GMC)') +
           facet_grid(Dose~st ) +
-          theme(panel.spacing = unit(1, "lines"))
-        p1
+          theme(panel.spacing = unit(1.5, "lines"))
+        )
+
+    })
         
-        
-        
-      }else{
-        plot.ds <- d2[(d2$vax %in% input$vax & 
+  
+    output$plot_ratio = renderPlotly({
+      plot.ds <- d2[(d2$vax %in% input$vax & 
                          d2$Dose %in% input$doses & 
                          d2$st %in% input$st &
                          d2$Trial %in% input$country ) ,]
         plot.ds.c <- reshape2::dcast(plot.ds, Dose+Trial+st ~vax, value.var='gmc')
         plot.ds.c <- plot.ds.c[!is.na(plot.ds.c$PCV13) & !is.na(plot.ds.c$PCV7),]
-        p2 <- ggplot(plot.ds.c, aes(x=st, y=log(PCV13/PCV7), group=Trial, col=Trial) ) +
+        p2 <- ggplotly(
+          ggplot(plot.ds.c, aes(x=st, y=log(PCV13/PCV7), group=Trial, col=Trial) ) +
           geom_point() +
           theme_classic()+
-          ylab('log(GMC)') +
+          ggtitle("Ratio of new vaccine to reference") +
+          ylab('log(Ratio)') +
           geom_line(aes(group = Trial),color="grey") +
           geom_hline(yintercept=0) +
           facet_grid(Dose~. ) +
           theme(panel.spacing = unit(1, "lines"))
-        p2
+        )
         
-      }
-      
-      
-    },width = "auto", height = "auto")
+      })
   }
 )
