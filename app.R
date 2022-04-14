449c5ca2f914ebe72e44b35583c6c337a9487f7a
weinbergerlab <dweinber@gmail.com>
2022-04-13 22:01:56 -0400

    ratio plot works

--- app.R
+++ app.R
@@ -4,6 +4,11 @@ library(reshape2)
 library(shiny)
 library(ggplot2)
 library(plotly)
+library(grid)
+library(ggsci)
+library(shinydashboard)
+
+
 
 pcv7sts <- c('4','6B','9V','14','18C','19F','23F')
 d1a <- read_excel("./Data/IgGGMCs2.xlsx")
@@ -19,6 +24,8 @@ d1.m <- d1.m[d1.m$variable %in% pcv7sts ,]
 d2 <- dcast(d1.m, vax+Dose+Trial+timepoint_dose_weeks+variable ~.)
 names(d2) <- c('vax','Dose','Trial','time_dose', 'st','gmc')
 
+d2$vax <- factor(d2$vax, levels=c('PCV7','PCV13','PCV20'))
+d2 <- d2[order(d2$vax),]
 
 shinyApp(
   
@@ -37,14 +44,16 @@ shinyApp(
                          unique(d2$Dose), selected=c("3rddoseP" ,"Booster")),
       checkboxGroupInput("st", "Serotypes:",
                          unique(d2$st),  selected=unique(d2$st)),
-      selectInput("ref_vax", "Vaccine to compare against:",
+      selectInput("ref_vax", "Reference vaccine:",
                   unique(d2$vax), multiple=F, selected=unique(d2$vax)[1]),
+      selectInput("comp_vax", "Comparator vaccine",
+                  unique(d2$vax), multiple=F, selected=unique(d2$vax)[2]),
      ),
     mainPanel(
       tabsetPanel(type = "tabs",
                   tabPanel("Concentration", plotlyOutput("plot_conc")),
-                  tabPanel("Ratio", plotlyOutput("plot_ratio"))
-      )
+                  tabPanel("Ratio", plotlyOutput("plot_ratio"), height='100%')
+      ), height=12
       )
   )
   ), 
@@ -82,22 +91,51 @@ shinyApp(
         vax.dat <- plot.ds.c[,input$vax, drop=F]
         vax.dat.ratio <- as.data.frame(apply(vax.dat,2, function(x) x/vax.dat[,input$ref_vax]))
         vax.dat.ratio <- vax.dat.ratio[, -grep(input$ref_vax, names(vax.dat.ratio) ), drop=F]
-        names(vax.dat.ratio) <- paste0('Numerator ', names(vax.dat.ratio))
+       # names(vax.dat.ratio) <- paste0('Numerator ', names(vax.dat.ratio))
         
         plot.ds.c2 <- cbind.data.frame(plot.ds.c[c('Dose','Trial','st')],vax.dat.ratio)
         plot.ds.c2.m <- reshape2::melt(plot.ds.c2, id.vars=c('Dose','Trial','st'))
 
-        p2 <- ggplotly(
-          ggplot(plot.ds.c2.m, aes(x=st, y=log(value), group=Trial, col=Trial) ) +
-          geom_point() +
-          theme_classic()+
-          ggtitle(paste0("Ratio of PCVx to ", input$ref_vax )) +
-          ylab('log(Ratio)') +
-          geom_line(aes(group = Trial),color="grey") +
-          geom_hline(yintercept=0) +
-          facet_grid(Dose~variable ) +
-          theme(panel.spacing = unit(1, "lines"))
+        dat_text <- data.frame(
+          label = c(rep('', length( unique(plot.ds.c2.m$st))-1) ,   paste0("Higher immunogenicity for ",  input$comp_vax)),
+          st   = unique(plot.ds.c2.m$st)
         )
+        
+        dat_text2 <- data.frame(
+          label = c(rep('', length( unique(plot.ds.c2.m$st))-1) ,   paste0("Higher immunogenicity for ", input$ref.vax)),
+          st   = unique(plot.ds.c2.m$st)
+        )
+        
+        
+        p2 <- ggplotly(
+          ggplot(plot.ds.c2.m[plot.ds.c2.m$variable==input$comp_vax,], aes(y=Trial, x=(value), col=st ) ) +
+            geom_point() +
+            theme_classic()+
+            ggtitle(paste0("Comparison of ", input$ref.vax, ' to ', input$comp_vax)) +
+            ylab('Study') +
+            xlab('Ratio of Immunogenicity')+
+            geom_vline(xintercept=1, lty=2, col='gray') +
+            xlim(0.2, 1.7) +
+            # ylim(0,NA) +
+            scale_color_lancet() +
+            facet_grid( rows = vars(st)) +
+            theme(panel.spacing = unit(0.5, "lines"))+
+            theme(legend.position="none") +
+            geom_text(
+              data    = dat_text,
+              mapping = aes(x = 1.4, y = 2, label = label),
+              hjust   = 0,
+              vjust   = 0.5,
+              col='gray'
+            ) +
+            geom_text(
+              data    = dat_text2,
+              mapping = aes(x = 0.5, y = 2, label = label),
+              hjust   = 1,
+              vjust   = 0.5,
+              col='gray'
+            )
+          )
         
       })
   }
