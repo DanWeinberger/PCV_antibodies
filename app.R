81aa9151a38634aa0a02e87ed049384be5fc2e14
weinbergerlab <dweinber@gmail.com>
2022-06-09 13:23:36 -0400

    updates to fit app with data

--- app.R
+++ app.R
@@ -22,7 +22,7 @@ names(d1) <- gsub('clinical_trial_','',names(d1))
 d1$Trial <- as.numeric(as.factor(d1$study_name))
 
 keep.vars <- c('vaccine','dose_number','Trial','location_continent',
-               'time_frame','standard_age_list','phase','assay','serotype', 'vaccine')
+               'time_frame','standard_age_list','phase','assay','serotype')
 
 d2 <- d1 %>% 
  select(all_of(c(keep.vars,'value')))
@@ -37,6 +37,12 @@ d2 <- d2 %>%
   mutate(total_doses=max(dose_number) ,Dose=paste0(dose_number,'/',total_doses) )
 
 
+#check for duplicates
+
+dups <- d1%>%
+  group_by(Trial, dose_number, vaccine, standard_age_list, assay, serotype) %>%
+  mutate(n_obs=n())
+
 shinyApp(
   
   ui = dashboardPage(
@@ -46,7 +52,7 @@ shinyApp(
                       selectInput("st", "Serotypes:",multiple=T,
                                   unique(d2$serotype),  selected=c('4','14','19F','23F')),
                       selectInput("doses", "Doses:",
-                                         unique(d2$Dose), selected=c("2/3")),
+                                         unique(d2$Dose), selected=c("2/3"), multiple=T),
                       selectInput("age", "Age group:",
                                   unique(d2$standard_age_list), selected=c("[\"Child\"]")),
                       selectInput("phase", "Trial Phase:",
@@ -64,12 +70,14 @@ shinyApp(
       fluidRow(
         tabBox(
           title = "",
-          id = "tabset1", height = "600px",
+          id = "tabset1", height = "600px",width=8,
           tabPanel("Concentration (GMC)", plotlyOutput("plot_gmc") ),
           tabPanel("Activity (OPA)", plotlyOutput("plot_opa"))
-        ),
+        )),
+      fluidRow(
+        
         box(
-          tabPanel("Ratio", plotlyOutput("plot_ratio", height='600px'))
+          tabPanel("Ratio", plotlyOutput("plot_ratio", height='600px',width=8))
         ),
         infoBox("Important information", "Data on immunogenicity alone cannot be used to infer differences in effectiveness between vaccines. These data need to be combined with information on the protective concentration of antibodies required to protect against each serotype in different populations for meaningful comparisons", icon = icon("glyphicon glyphicon-exclamation-sign",lib ='glyphicon'), width=12),
         
@@ -142,26 +150,34 @@ shinyApp(
         plot.ds.c <- reshape2::dcast(plot.ds, Dose+Trial+serotype +assay~vaccine, value.var='value')
         
         vax.dat <- plot.ds.c[,names(plot.ds.c) %in% as.character(unique(d2$vaccine)), drop=F]
+        
         vax.dat.ratio <- as.data.frame(apply(vax.dat,2, function(x) x/vax.dat[,input$ref_vax]))
-        vax.dat.ratio <- vax.dat.ratio[, -grep(input$ref_vax, names(vax.dat.ratio) ), drop=F]
-       # names(vax.dat.ratio) <- paste0('Numerator ', names(vax.dat.ratio))
+        
+        vax.dat.ratio <- vax.dat.ratio[, -which(ref_vax==names(vax.dat.ratio) ), drop=F]
+        
+        # names(vax.dat.ratio) <- paste0('Numerator ', names(vax.dat.ratio))
         
         plot.ds.c2 <- cbind.data.frame(plot.ds.c[c('Dose','Trial','serotype','assay')],vax.dat.ratio)
         plot.ds.c2.m <- reshape2::melt(plot.ds.c2, id.vars=c('Dose','Trial','serotype','assay'))
-
+        
+        plot.df <- plot.ds.c2.m[plot.ds.c2.m$variable==input$comp_vax & plot.ds.c2.m$assay=='IgG',]
+        
+        plot.df$Trial <- as.numeric( as.factor(plot.df$Trial))
+        
+        plot.df <- plot.df[!is.na(plot.df$value),]
+        
         dat_text <- data.frame(
-          label = c(rep('', length( unique(plot.ds.c2.m$st))-1) ,   paste0("Higher immunogenicity for ",  input$comp_vax)),
-          st   = unique(plot.ds.c2.m$st)
+          label = c(rep('', length( unique(plot.df$serotype))-1) ,   paste0("Higher immunogenicity for ",  input$comp_vax)),
+          serotype   = unique(plot.df$serotype)
         )
         
         dat_text2 <- data.frame(
-          label = c(rep('', length( unique(plot.ds.c2.m$st))-1) ,   paste0("Higher immunogenicity for ", input$ref_vax)),
-          st   = unique(plot.ds.c2.m$st)
+          label = c(rep('', length( unique(plot.df$serotype))-1) ,   paste0("Higher immunogenicity for ", input$ref_vax)),
+          serotype   = unique(plot.df$serotype)
         )
         
-        
-        p2 <- ggplotly(
-          ggplot(plot.ds.c2.m[plot.ds.c2.m$variable==input$comp_vax & plot.ds.c2.m$assay=='IgG',], aes(y=Trial, x=(value), col=serotype ) ) +
+                p2 <- ggplotly(
+          ggplot(plot.df, aes(y=Trial, x=(value), col=serotype ) ) +
             geom_point() +
             theme_classic()+
             ggtitle(paste0("Comparison of ", input$ref_vax, ' to ', input$comp_vax)) +
@@ -171,24 +187,27 @@ shinyApp(
             xlim(0.2, 1.7) +
             # ylim(0,NA) +
             scale_color_lancet() +
-            facet_grid( rows = vars(st)) +
+            facet_grid( rows = vars(serotype)) +
             theme(panel.spacing = unit(0.5, "lines"))+
             theme(legend.position="none") +
             geom_text(
               data    = dat_text,
-              mapping = aes(x = 1.4, y = 2, label = label),
+              mapping = aes(x = 1.4, y = 1.5, label = label),
               hjust   = 0,
               vjust   = 0.5,
               col='gray'
             ) +
             geom_text(
               data    = dat_text2,
-              mapping = aes(x = 0.5, y = 2, label = label),
+              mapping = aes(x = 0.5, y = 1.5, label = label),
               hjust   = 1,
               vjust   = 0.5,
               col='gray'
-            )
-          )
+            )+
+            theme(axis.title.y=element_blank(),
+                  axis.text.y=element_blank(),
+                  axis.ticks.y=element_blank())
+        )
       })
   }
 )
